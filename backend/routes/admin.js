fdd9e0f30dd515bef72c08f3dd8130cc02048a56
ShubhamMore6688 <shubhamdmore03@gmail.com>
2024-12-15 07:48:48 +0530

    updating routes

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,9 +1,10 @@
 import express from "express";
+import { addPackage, deletePackage, updatePacakge } from "../controllers/adminController.js";
 
 const router = express.Router();
 
-router.get('/packages') // add new package
-router.put('/packages/:id') // update package
-router.delete('/packages/:id') // delete package
+router.get('/packages', addPackage) // add new package
+router.put('/packages/:id', updatePacakge) // update package
+router.delete('/packages/:id', deletePackage) // delete package
 
 export default router
--- backend/routes/package.js
+++ backend/routes/package.js
@@ -1,9 +1,11 @@
 import express from "express";
+import { getAllPackages, getPackageById } from "../controllers/packageController.js";
+import { createBooking } from "../controllers/bookingController.js";
 
 const router = express.Router();
 
-router.get('/packages') // all packages
-router.get('/packages/:id') // perticular package detail
-router.post('/bookings') // add post details
+router.get('/packages', getAllPackages) // all packages
+router.get('/packages/:id', getPackageById) // perticular package detail
+router.post('/bookings', createBooking ) // add post details
 
 export default router;
