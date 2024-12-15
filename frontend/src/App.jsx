c2166ffb0d4dff24d4fcf80ecf16d0becbe8bd29
ShubhamMore6688 <shubhamdmore03@gmail.com>
2024-12-15 19:29:18 +0530

    adding routes for admin in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PackageDetails from "./pages/packageDetails";
 import BookingForm from "./pages/bookingForm";
 import InvoicePage from "./pages/InvoicePage";
+import AdminPanel from "./components/AdminPanel";
+import PackagesList from "./components/PackagesList";
+import PackageForm from "./components/PackageForm";
 
 function App() {
   
@@ -13,6 +16,11 @@ function App() {
         <Route path="/packages/:id" element={<PackageDetails/>} />
         <Route path="/bookings" element={<BookingForm/>} />
         <Route path="/invoice/:bookingId" element={<InvoicePage/>} />
+
+        <Route path="/admin" element={<AdminPanel />} />
+        <Route path="/admin/packages" element={<PackagesList />} />
+        <Route path="/admin/packages/add" element={<PackageForm />} />
+        <Route path="/admin/packages/edit/:id" element={<PackageForm />} />
       </Routes>
     </Router>
   )
